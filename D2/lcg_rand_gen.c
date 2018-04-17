b2ac1b2f830a70793768daa919482692d71b575d
rajat <rjtkpanda@gmail.com>
2018-04-17 15:02:16 +0200

    added the working lcg random generator

--- D2/lcg_rand_gen.c
+++ D2/lcg_rand_gen.c
@@ -2,20 +2,22 @@
 #include "stdio.h"
 #include "unistd.h"
 
-int lcg(int m, int a, int c, int x0, int n);
+int lcg(int m, int a, int c, int x0);
 
 int main(int argc, char *argv[]) {
-  lcg(83, 7, 3, 1, 30);
-  // lcg(83, 7, 0, 1, 30); // mlcg random number generator
+  unsigned int x = 16807;      /* 7^5 */
+  unsigned int m = 2147483647; /* 2^31 -1 */
+  lcg(m, x, 3, 1);
+  // lcg(m, x, 0, 1); // mlcg random number generator
   return 0;
 }
 
-int lcg(int m, int a, int c, int x0, int n) {
+int lcg(int m, int a, int c, int x0) {
   int x;
-  for (int i = 0; i < n; i++) {
+  while (1) {
     x = a * x0 + c;
     x0 = x % m;
-    printf("%i\n", x0);
+    write(1, &x0, 4);
   }
   return 0;
 }
